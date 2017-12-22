72093a7c84a8f9ee0cd7a2af78ac9292bc19f64e
Malcolm Wright <wrightmbw01@gmail.com>
2017-12-22 18:38:55 -0500

    Finished the barriers and the player bullet shooting. Added the score and live count. Started to work on drawing the aliens.

--- GameTemplate/Screens/GameScreen.cs
+++ GameTemplate/Screens/GameScreen.cs
@@ -20,8 +20,6 @@ namespace GameTemplate.Screens
         bool bulletOnScreen = false;
         bool leftKeyDown, shootKeyDown, rightKeyDown, exit;
 
-        Point[] alienPoints;
-
         // Graphics
         Graphics offScreen;
         Graphics onScreen;
@@ -33,9 +31,14 @@ namespace GameTemplate.Screens
              barrier1, barrier2, barrier3, barrier4;
         // we can change the barrier image depending on the health
 
-        Rectangle playerRect, barrier1Rect,
+        Rectangle bullet, playerRect, barrier1Rect,
             barrier2Rect, barrier3Rect, barrier4Rect;
 
+        Rectangle[] alienRow1;
+        Rectangle[] alienRow2;
+        Rectangle[] alienRow3;
+        Rectangle[] alienRow4;
+        Rectangle[] alienRow5;
 
         // sounds and images
         SoundPlayer playerBullet, alienBullet, alienHit, playerHit,
@@ -86,7 +89,15 @@ namespace GameTemplate.Screens
             barrier3Rect.Width = 72;
             barrier3Rect.Height = 54;
 
-            // barrier4
+            barrier4Rect.X = 800 - (barrier1Rect.Width / 2);
+            barrier4Rect.Y = 450;
+            barrier4Rect.Width = 72;
+            barrier4Rect.Height = 54;
+
+            bullet.X = 0;
+            bullet.Y = 0;
+            bullet.Width = 1;
+            bullet.Height = 6;
 
             // load sounds
             playerBullet = new SoundPlayer(Properties.Resources.player_shoot);
@@ -102,6 +113,17 @@ namespace GameTemplate.Screens
             barrier1 = new Bitmap(Properties.Resources.coverFull);
             barrier2 = barrier1;
             barrier3 = barrier1;
+            barrier4 = barrier1;
+
+            alienRow1 = new Rectangle[11];
+
+            for (int i = 0; i < alienRow1.Length; i++)
+            {
+                alienRow1[i].X = 25 + (75 * i);
+                alienRow1[i].Y = 50;
+                alienRow1[i].Width = 36;
+                alienRow1[i].Height = 24;
+            }
         }
 
         #region required global values - DO NOT CHANGE
@@ -274,7 +296,30 @@ namespace GameTemplate.Screens
                 playerRect.X += PLAYER_SPEED;
             }
 
+           if (spaceDown && !bulletOnScreen)
+            { 
+                bulletOnScreen = true;
+                playerBullet.Play();
+
+                // move the bullet over the player
+                bullet.X = playerRect.X + (playerRect.Width / 2);
+                bullet.Y = playerRect.Y - 15;
+            }
+
+           if (bulletOnScreen)
+            {
+                bullet.Y -= BULLET_SPEED;
+            }
+
+           if (bullet.Y <= 0)
+            {
+                bulletOnScreen = false;
+            }
+
+
             #endregion
+
+
 
             #region monster movements - TO BE COMPLETED
 
@@ -282,8 +327,90 @@ namespace GameTemplate.Screens
             #endregion
 
             #region collision detection - TO BE COMPLETED
-            
+            if (bullet.IntersectsWith(barrier1Rect) && barrier1health != 0)
+            {
+                bulletOnScreen = false;
+                barrier1health--;
 
+                // reset the x
+                bullet.X = 0;
+            }
+            if (bullet.IntersectsWith(barrier2Rect) && barrier2health != 0)
+            {
+                bulletOnScreen = false;
+                barrier2health--;
+                // reset the x
+                bullet.X = 0;
+            }
+            if (bullet.IntersectsWith(barrier3Rect) && barrier3health != 0)
+            {
+                bulletOnScreen = false;
+                barrier3health--;
+                // reset the x
+                bullet.X = 0;
+            }
+            if (bullet.IntersectsWith(barrier4Rect) && barrier4health != 0)
+            {
+                bulletOnScreen = false;
+                barrier4health--;
+                // reset the x
+                bullet.X = 0;
+            }
+
+            #endregion
+
+            #region barrier logic
+            if (barrier1health == 12)
+            {
+                barrier1 = new Bitmap(Properties.Resources.coverDmg1);
+            }
+            else if (barrier1health == 8)
+            {
+                barrier1 = new Bitmap(Properties.Resources.coverDmg2);
+            }
+            else if (barrier1health == 4)
+            {
+                barrier1 = new Bitmap(Properties.Resources.coverDmg3);
+            }
+
+            if (barrier2health == 12)
+            {
+                barrier2 = new Bitmap(Properties.Resources.coverDmg1);
+            }
+            else if (barrier2health == 8)
+            {
+                barrier2 = new Bitmap(Properties.Resources.coverDmg2);
+            }
+            else if (barrier2health == 4)
+            {
+                barrier2 = new Bitmap(Properties.Resources.coverDmg3);
+            }
+
+            if (barrier3health == 12)
+            {
+                barrier3 = new Bitmap(Properties.Resources.coverDmg1);
+            }
+            else if (barrier3health == 8)
+            {
+                barrier3 = new Bitmap(Properties.Resources.coverDmg2);
+            }
+            else if (barrier3health == 4)
+            {
+                barrier3 = new Bitmap(Properties.Resources.coverDmg3);
+            }
+
+            if (barrier4health == 12)
+            {
+                barrier4 = new Bitmap(Properties.Resources.coverDmg1);
+            }
+            else if (barrier4health == 8)
+            {
+                barrier4 = new Bitmap(Properties.Resources.coverDmg2);
+            }
+            else if (barrier4health == 4)
+            {
+                barrier4 = new Bitmap(Properties.Resources.coverDmg3);
+            }
             #endregion
 
 
@@ -318,33 +445,53 @@ namespace GameTemplate.Screens
         /// <param name="e"></param>
         private void GameScreen_Paint(object sender, PaintEventArgs e)
         {
+            e.Graphics.Clear(Color.Black);
+
             //draw rectangle to screen
             e.Graphics.DrawImage(player, playerRect.X, playerRect.Y, 
                 playerRect.Width, playerRect.Height);
 
-            if (barrier1health == 16)
+            e.Graphics.DrawString("Score: " + score, subFont,
+                solidBrush, 25, 25);
+
+            e.Graphics.DrawString("Lives: " + lives, subFont,
+                solidBrush, Width - 160, 25);
+
+            if (barrier1health != 0)
             {
                 e.Graphics.DrawImage(barrier1, barrier1Rect.X, barrier1Rect.Y,
                     barrier1Rect.Width, barrier1Rect.Height);                   
             }
-            if (barrier2health == 16)
+
+            if (barrier2health != 0)
             {
                 e.Graphics.DrawImage(barrier2, barrier2Rect.X,
                     barrier2Rect.Y, barrier2Rect.Width,
                     barrier2Rect.Height);
             }
-            if (barrier3health == 16)
+            if (barrier3health != 0)
             {
                 e.Graphics.DrawImage(barrier3, barrier3Rect.X,
                     barrier3Rect.Y, barrier3Rect.Width,
                     barrier3Rect.Height);
             }
-            /*if (barrier3health == 16)
+
+            if (barrier4health != 0)
             {
-                e.Graphics.DrawImage(barrier2, barrier2Rect.X,
-                    barrier2Rect.Y, barrier2Rect.Width,
-                    barrier2Rect.Height);
-            } */
+                e.Graphics.DrawImage(barrier4, barrier4Rect.X,
+                    barrier4Rect.Y, barrier4Rect.Width,
+                    barrier4Rect.Height);
+            } 
+
+            if (bulletOnScreen)
+            {
+                e.Graphics.DrawRectangle(pen, bullet);
+            }
+
+            foreach (Rectangle alien in alienRow1)
+            {
+                e.Graphics.DrawImage(alien1, alien);
+            }
         }
     }
 }
